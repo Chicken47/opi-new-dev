d31b9fb47db5671d97a164e9d97281f5941c31a2
Chicken47 <akshit.aryan1999@gmail.com>
2023-06-11 16:49:45 +0530

    lmfao

--- src/pages/aboutPage/AboutShort.jsx
+++ src/pages/aboutPage/AboutShort.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const AboutShort = () => {
   return (
-    <div className="w-1/2 flex items-center justify-center overflow-hidden">
-      <img src="/images/png/bgimg1.png" className="" alt="" />
+    <div className="w-1/2 overflow-hidden flex justify-center">
+      <img src="/images/png/bgimg1.png" className="w-4/5 object-cover" alt="" />
     </div>
   );
 };
