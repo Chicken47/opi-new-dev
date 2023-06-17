363d0d2e2379ed7b9bcaff0e476c566ad55ab916
Chicken47 <akshit.aryan1999@gmail.com>
2023-06-17 22:03:04 +0530

    sdfsdf

--- src/pages/contactPage/ContactForm.jsx
+++ src/pages/contactPage/ContactForm.jsx
@@ -92,34 +92,34 @@ const ContactForm = () => {
             </span>
           </div>
         </div>
-        <div className="p-5 w-4/5 border md:w-2/5 border-black flex flex-col mt-10">
-          <span className="font-mont text-black mt-7">Name</span>
+        <div className="p-5 w-4/5 border md:w-2/5 border-ipink flex flex-col mt-10">
+          <span className="font-mont text-pblue mt-7">Name</span>
           <input
             type="text"
-            className="px-3 py-1 border border-black mt-2"
+            className="px-3 py-1 border border-oblue mt-2"
             placeholder=""
           />
-          <span className="font-mont text-black mt-10">Number</span>
+          <span className="font-mont text-pblue mt-10">Number</span>
           <input
             type="text"
-            className="px-3 py-1 border border-black mt-2"
+            className="px-3 py-1 border border-oblue mt-2"
             placeholder=""
           />
-          <span className="font-mont text-black mt-10">e-Mail</span>
+          <span className="font-mont text-pblue mt-10">e-Mail</span>
           <input
             type="text"
-            className="px-3 py-1 border border-black mt-2"
+            className="px-3 py-1 border border-oblue mt-2"
             placeholder=""
           />
-          <span className="font-mont text-black mt-10">
+          <span className="font-mont text-pblue mt-10">
             Comments on enquiry
           </span>
           <input
             type="text"
-            className="px-3 py-1 border border-black mt-2"
+            className="px-3 py-1 border border-oblue mt-2"
             placeholder=""
           />
-          <div className="px-5 py-2 mt-10 cursor-pointer hover:text-white hover:bg-black transition-all duration-500 border border-black font-mont text-center md:w-1/5">
+          <div className="px-5 py-2 mt-10 cursor-pointer hover:text-white hover:bg-pblue transition-all duration-500 border border-pblue font-mont text-center md:w-1/5">
             Submit
           </div>
         </div>
